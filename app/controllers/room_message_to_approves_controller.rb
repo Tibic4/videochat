20c66baef178e156a17c106f52221e5f43de6e3b
Alton Vieira <bicagold@gmail.com>
2022-10-15 13:18:48 -0300

    refactor: rooms stylesheets

--- app/controllers/room_message_to_approves_controller.rb
+++ app/controllers/room_message_to_approves_controller.rb
@@ -1,9 +1,12 @@
 class RoomMessageToApprovesController < ApplicationController
   protect_from_forgery with: :null_session
 
+  def index
+    @room = Room.find_by(name: params[:name])
+  end
+
   def refuse_message
     message = RoomMessageToApprove.find(params[:id])
     message.destroy
   end
-
 end
